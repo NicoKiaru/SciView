0d93bd91c808373c40791f534bde8c592df7f212
Kyle Harrington <kharrington@uidaho.edu>
2016-12-09 18:43:24 +0100

    Maybe we can do this without the updater?

--- threedviewer_deploy.sh
+++ threedviewer_deploy.sh
@@ -3,12 +3,11 @@
 if [[ 1 == 1 ]]
 then
    mvn deploy --settings settings.xml
-   curl -O http://downloads.imagej.net/fiji/latest/fiji-nojre.zip
-   unzip fiji-nojre.zip
-   mv target/ThreeDViewer* Fiji.app/jars/
-   cp -r src/plugins/* Fiji.app/plugins/
-   cd Fiji.app
-   curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh   
-   chmod a+x upload-site-simple.sh
-   ./upload-site-simple.sh ThreeDViewer ThreeDViewer
+   #curl -O http://downloads.imagej.net/fiji/latest/fiji-nojre.zip
+   #unzip fiji-nojre.zip
+   #mv target/ThreeDViewer* Fiji.app/plugins/
+   #cd Fiji.app
+   #chmod a+x upload-site-simple.sh
+   #./upload-site-simple.sh ThreeDViewer ThreeDViewer
+   curl -T target/ThreeDViewer-0.0.1-SNAPSHOT.jar -u ThreeDViewer:$WIKI_UPLOAD_PASS http://sites.imagej.net/ThreeDViewer/plugins/ThreeDViewer-0.0.1-SNAPSHOT.jar
 fi
